8ee9f7d4ba9bd24fbbf7412d88acc1049d06df9e
Damian Mooyman <damian.mooyman@pixelfusion.co.nz>
2024-10-02 22:45:46 +1300

    fix: restore isr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import fetchData from "@/lib/shaleDb";
 import TierList from "@/components/TierList/TierList";
 
-// export const revalidate = 600 // invalidate every 5 minutes
+export const revalidate = 600 // invalidate every 5 minutes
 
 const Home = async () => {
   const data = await fetchData()
