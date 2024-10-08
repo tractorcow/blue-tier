218a7eb6aad3e97a0c0da59d20c9734d3fc4faa3
Fauzul Ashari <p4ujul@gmail.com>
2024-10-08 20:43:35 +0700

    feat: Add student armor type

--- src/components/Students/StudentCard.tsx
+++ src/components/Students/StudentCard.tsx
@@ -8,21 +8,70 @@ export interface StudentCardProps {
 
 const StudentCard = ({ student }: StudentCardProps) => {
   const iconImage = `https://cdn.jsdelivr.net/gh/SchaleDB/SchaleDB@main/images/student/icon/${student.Id}.webp`
+  const defenseIcon =
+    'https://raw.githubusercontent.com/SchaleDB/SchaleDB/refs/heads/main/images/ui/Type_Defense.png'
+
+  const bulletClasses: { [key: string]: string } = {
+    Pierce: 'bg-[#986C04]',
+    Explosion: 'bg-[#7C0405]',
+    Mystic: 'bg-[#145A86]',
+    Sonic: 'bg-[#784294]',
+  }
+
+  const armorClasses: { [key: string]: string } = {
+    Normal: 'bg-gray-500',
+    LightArmor: 'bg-[#7C0405]',
+    HeavyArmor: 'bg-[#986C04]',
+    Unarmed: 'bg-[#145A86]',
+    ElasticArmor: 'bg-[#784294]',
+  }
+
+  const bulletTypes = bulletClasses[student.BulletType] || ''
+  const armorTypes = armorClasses[student.ArmorType] || ''
+
   return (
     <div
       key={student.Id}
-      className='flex h-28 w-24 flex-col items-center rounded bg-gray-300 p-2 dark:bg-gray-600'
+      className={`flex h-40 w-28 flex-col items-center rounded-lg p-2 shadow-lg ${bulletTypes} cursor-grab`}
     >
-      <Image
-        src={iconImage}
-        alt={student.Name}
-        width={64}
-        height={64}
-        className='rounded'
-      />
-      <span className='mt-2 text-center text-sm text-gray-800 dark:text-gray-200'>
+      <div className='relative'>
+        <div
+          className={`w-auto rounded ${armorTypes} absolute left-0 top-0 flex justify-center border-2 border-gray-700 shadow-lg`}
+        >
+          <Image
+            src={defenseIcon}
+            alt='asd'
+            width={32}
+            height={32}
+            className='rounded p-1'
+          />
+          {/* Add bullet type icon if available */}
+        </div>
+
+        <Image
+          src={iconImage}
+          alt={student.Name}
+          width={72}
+          height={64}
+          className='rounded object-fill'
+        />
+        {/* Optional bullet type icon in the top-left corner */}
+      </div>
+      <span className='my-2 overflow-hidden text-clip text-center text-base text-gray-200'>
         {student.Name}
       </span>
+      {/* Empty div below the name */}
+      <div
+        className={`w-full rounded ${armorTypes} flex justify-center border-2 border-gray-700 shadow-lg`}
+      >
+        <Image
+          src={defenseIcon}
+          alt='asd'
+          width={32}
+          height={32}
+          className='rounded p-1'
+        />
+      </div>
     </div>
   )
 }
